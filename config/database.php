382b7a173c0aa765a1d32d7011504cec84a47e73
David Rey G <neisserrey@upeu.edu.pe>
2020-05-26 10:25:11 -0500

    base de datos en postgres

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url(env('DATABASE_URL'));
+$host = $DATABASE_URL["host"] ?? null;
+$username = $DATABASE_URL["user"] ?? null;
+$password = $DATABASE_URL["pass"] ?? null;
+$database = substr($DATABASE_URL["path"], 1);
 return [
 
     /*
@@ -66,11 +71,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -123,7 +128,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
