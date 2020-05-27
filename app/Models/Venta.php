c30f780470f0be2786b4445c6d0a9ffd261bdc62
David Rey G <neisserrey@upeu.edu.pe>
2020-05-27 10:14:38 -0500

    configurando venta.php

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -3,13 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Venta extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    public $fillable = [
+        'total',
+        'user_id',
+        'cliente_id'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'total'      => 'integer',
+        'user_id'    => 'integer',
+        'cliente_id' => 'integer',
+    ];
 
     public static $rules = [
-        'nombre' => 'required|string',
-        'tabla' => 'string|nullable',
-        'tamaño' => 'required|numeric'
+        'total' => 'required|numeric',
+        'user_id' => 'required',
+        'user_id'  => 'required|exists:usuarios,id,deleted_at,NULL',
+        'cliente_id'  => 'required|exists:clientes,id,deleted_at,NULL'
     ];
 }
